cd261543c709fc336923a681938d4466c7e1d49e
tageh <tage.hisdal@gmail.com>
2021-02-02 12:49:44 +0100

    edit

--- oblig/PB1090-oblig1-237187/Oblig1Oppgave1_237187.java
+++ oblig/PB1090-oblig1-237187/Oblig1Oppgave1_237187.java
@@ -1,30 +1,17 @@
 import java.util.*;
 
-import jdk.internal.org.jline.reader.SyntaxError;
-
 public class Oblig1Oppgave1_237187{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Write the coordiates: ! \n");
-        System.out.println("x1: ");
+        System.out.println("Write the coordiates: x1, y1, x2, y2, x3, y3, x4, y4 ! \n");
         double x1 = input.nextDouble();
-        System.out.println("y1: ");
         double y1 = input.nextDouble();
-
-        System.out.println("x2: ");
         double x2 = input.nextDouble();
-        System.out.println("y2: ");
         double y2 = input.nextDouble();
-
-        System.out.println("x3: ");
         double x3 = input.nextDouble();
-        System.out.println("y3: ");
         double y3 = input.nextDouble();
-
-        System.out.println("x4: ");
-        double x4= input.nextDouble();
-        System.out.println("y4: ");
+        double x4 = input.nextDouble();
         double y4 = input.nextDouble();
 
 
@@ -38,11 +25,9 @@ public class Oblig1Oppgave1_237187{
         LinearEquation Le = new LinearEquation(a,b,c,d,e,f);
         
         Le.printIntersectionPoint();
-
+        
+        input.close();
     }
-
-    
-
 
 }
 
